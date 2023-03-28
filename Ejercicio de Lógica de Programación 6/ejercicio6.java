a96b730805b3cbd8a35ba159f28bd03abb38d53d
Rafael Encinas <rafaeladalbertoencinas94@gmail.com>
2023-03-28 11:43:17 -0700

    Se agregaron comentarios al codigo para mejor comprension

--- Ejercicio de Lógica de Programación 6/ejercicio6.java
+++ Ejercicio de Lógica de Programación 6/ejercicio6.java
@@ -13,11 +13,21 @@ public class Ejercicio6 {
 	public static void reverseString() {
 		System.out.println("Este programa recibe una cadena ingresada por el usuario y la inverte.");
 		System.out.println("Por favor ingrese la cadena que desea invertir:");
+		//Se crea un scanner para que el usuario pueda ingresar la cadena de caracteres
 		Scanner input = new Scanner(System.in);
+		
+		//Se crea una variable del tipo String donde se almacena lo que el usuario ingresa en el scanner
 		String cadena = input.nextLine();
+		
+		//Se crea una variable StringBuilder ya que esta contiene el metodo .reverse() que nos
+		//permite invertir cadenas
 		StringBuilder cadenaReversed = new StringBuilder();
+		
+		//Se agrega la cadena original a la cadena StringBuilder con .append()
 		cadenaReversed.append(cadena);
 		System.out.println("Cadena invertida resultante:");
+		
+		//Se aplica el metodo .reverse() y se muestra aqui mismo el resultado
 		System.out.println(cadenaReversed.reverse());	
 	}
 
